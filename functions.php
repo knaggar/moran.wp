6f9ca9801906795f780a8c4112f55770e8399028
Kareem A. Eid <kareematif@gmail.com>
2016-08-17 01:31:41 +0200

    plugin added

--- functions.php
+++ functions.php
@@ -3,5 +3,17 @@
 * @package moran
 * @subpackage functions
 * @version 1.5
-* Description: contains functions and definations for theme and other extra plugins (found in 'includes' folder).
+* Description: contains functions and definations for theme and other extra plugins (found in 'includesludes' folder).
 */
+
+// Theme plugins in 'includesludes' folder
+  // add theme customizer
+  require get_template_directory() . '/includes/wp_customizer.php';
+  // Clean html head
+  require get_template_directory() . '/includes/wp_clean-head.php';
+  // Add Custom Post Type
+  require get_template_directory() . '/includes/wp_custom-post-type.php';
+  // Add Custom Taxonomies
+  require get_template_directory() . '/includes/wp_custom-taxonomy.php';
+  // Add Share Buttons
+  require get_template_directory() . '/includes/wp_social-share.php';
